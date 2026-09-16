1fed8fe7e3aa17a4bd9ab435e42e9f807ca9f10e
williamjamesmoriarty-13 <williamjamesmoriart@gmail.com>
2026-09-16 10:06:06 +0000

    version [skip ci]

--- student-service/entrypoint.py
+++ student-service/entrypoint.py
@@ -5,10 +5,10 @@ import psycopg2  # Or your DB driver
 
 # Wait for PostgreSQL to be ready
 def wait_for_db():
-    print("[enrollment-service] Application des migrations Alembic...")
+    print("[students-service] Application des migrations Alembic...")
     subprocess.run(["alembic", "upgrade", "head"], check=True)
 
-    print("[enrollment-service] Démarrage de WSGI...")
+    print("[students-service] Démarrage de WSGI...")
     subprocess.run(["gunicorn", "-b", "0.0.0.0:5001", "wsgi:app"], check=True)
 
 
